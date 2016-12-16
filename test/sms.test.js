d4d4b7b469a21e9d765dff03db3ba29b60d0196f
503945930 <503945930@qq.com>
2016-12-16 10:39:56 +0800

    update rabbitmq

--- test/sms.test.js
+++ test/sms.test.js
@@ -1,21 +1,20 @@
 import test from 'ava';
-import {clientOption, smsOption} from '../config/config'
 import Alidayu from '../lib/sms';
 
-// const clientOption = {
-//   'appkey': '23569268',
-//   'appsecret': '57ddd04eebae351ec4afee97c8d75ac4',
-//   'REST_URL': 'gw.api.taobao.com/router/rest'
-// }
-//
-// const smsOption = {
-//   'extend': '',
-//   'sms_type': 'normal',
-//   'sms_free_sign_name': '夜点',
-//   'sms_param': '',
-//   'rec_num': '',
-//   'sms_template_code': 'SMS_34335476'
-// }
+const clientOption = {
+  'appkey': '23569268',
+  'appsecret': '57ddd04eebae351ec4afee97c8d75ac4',
+  'REST_URL': 'gw.api.taobao.com/router/rest'
+}
+
+const smsOption = {
+  'extend': '',
+  'sms_type': 'normal',
+  'sms_free_sign_name': '夜点',
+  'sms_param': '',
+  'rec_num': '',
+  'sms_template_code': 'SMS_34335476'
+}
 
 let alidayu;
 test.before(t => {
