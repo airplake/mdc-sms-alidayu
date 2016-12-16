e2172a04cc5192a53b83f22fa35353960ea1f277
503945930 <503945930@qq.com>
2016-12-16 19:01:10 +0800

    add Description

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ##夜点－短信服务包
 
 
+## run
+
+
+
+
 ## run test
 
     npm install --global ava
--- test/comsume.test.js
+++ test/comsume.test.js
@@ -1,18 +1,16 @@
 const AmqpConnection = require('../lib/rabbitmq')
 
 const clientOption = {
-    'appkey': '23569268',
-    'appsecret': '57ddd04eebae351ec4afee97c8d75ac4',
+    'appkey': '23569268',//appkey
+    'appsecret': '57ddd04eebae351ec4afee97c8d75ac4',//appsecret
     'REST_URL': 'gw.api.taobao.com/router/rest'
 }
 
 const smsOption = {
-    'extend': '',
-    'sms_type': 'normal',
-    'sms_free_sign_name': '夜点',
-    'sms_param': '',
-    'rec_num': '',
-    'sms_template_code': 'SMS_34335476'
+    'extend': '', //可选
+    'sms_type': 'normal', //短信类型，传入值请填写normal
+    'sms_free_sign_name': '夜点',//短信签名
+    'sms_template_code': 'SMS_34335476'//短信模板ID
 }
 
 let amqpConnection = new AmqpConnection('amqp://localhost',{
